d3fa15e918c674a3863b9c290264ed62adf69bf6
olubunmi <bjemiyor@gmail.com>
2022-07-01 10:41:21 +0000

    if the last digit of n is greater than 5: print the string and is greater than 5. if equas 0: the string and is 0. if less than 6 and not 0: the string and is less than 6 and not 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ *  main - finds a random number
+ *
+ *  Description: Returns is Positive or is negative or 0
+ *  Return: 0
+ */
 int main(void)
 {
 	int n;
